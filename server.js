d858cab274ce930c96a69b3df1c93d2a2d2396d8
Jason Fang <n10755888@qut.edu.au>
2023-11-09 10:58:31 +1000

    Removed hard-coded AWS config details.

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ app.use(cors());
 const AWS = require("aws-sdk");
 
 AWS.config.update({
-	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-	sessionToken: process.env.AWS_SESSION_TOKEN,
 	region: "ap-southeast-2",
 });
 
